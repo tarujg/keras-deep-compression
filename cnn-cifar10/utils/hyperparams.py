cf891259eff86d5e53ff2eb5d675b59b1425e67a
Taruj Goyal <taruj95@gmail.com>
2020-04-23 23:39:42 -0400

    Argparse Bool values

--- cnn-cifar10/utils/hyperparams.py
+++ cnn-cifar10/utils/hyperparams.py
@@ -10,10 +10,18 @@ def parse_args():
                         help='Number of epochs, typically passes through the entire dataset')
     parser.add_argument('--lr', default=0.001, type=float,
                         help='Learning rate for the optimization algorithm')
-    parser.add_argument('--add_maxpool', action='store_true', default=True,
+
+    parser.add_argument('--add_maxpool', dest='add_maxpool', action='store_true',
                         help='Add MaxPool layers to Program')
-    parser.add_argument('--data_aug', action='store_true', default=True,
+    parser.add_argument('--no-add_maxpool', dest='add_maxpool', action='store_false',
+                        help='Add MaxPool layers to Program')
+    parser.set_defaults(add_maxpool=False)
+    parser.add_argument('--data_aug', dest='data_aug', action='store_true',
+                        help='Add MaxPool layers to Program')
+    parser.add_argument('--no-data_aug', dest='data_aug', action='store_false',
                         help='Data augmentation for the model')
+    parser.set_defaults(data_aug=False)
+
     parser.add_argument('--optimizer', default='SGD', type=str, choices=['SGD', 'RMSprop', 'Adam'],
                         help='Options of optimizer to be used')
 
